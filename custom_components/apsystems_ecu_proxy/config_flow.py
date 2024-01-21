6eb0cb5b99e9fa57200eb0bb7f37467c1eefabbd
HAEdwin <82239374+HAEdwin@users.noreply.github.com>
2024-01-21 10:07:59 +0100

    Create config_flow.py

---
+++ custom_components/apsystems_ecu_proxy/config_flow.py
@@ -0,0 +1,61 @@
+import logging
+
+import voluptuous as vol
+
+from homeassistant import config_entries, core, exceptions
+
+
+from .const import DOMAIN
+from .__init__ import async_start_proxy, APSystemsECUProxyInvalidData
+
+
+_LOGGER = logging.getLogger(__name__)
+
+# Schema heeft alleen de naam van de host nog niets anders
+DATA_SCHEMA = vol.Schema({"host": str})
+
+
+async def validate_input(hass: core.HomeAssistant, data):
+    """Validate the user input allows us to connect."""
+    _LOGGER.warning(f"step 1: validate_input from config_flow.py data={data}")
+    try:
+        await async_start_proxy(data)
+    except APSystemsECUProxyInvalidData as err:
+        _LOGGER.debug(f"Error returned from async_start_proxy: {err}")
+        #raise friendly error
+        raise CannotConnect
+    return {"title": "APSystems ECU Proxy"}        
+
+
+class DomainConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for warmup4ie."""
+
+    VERSION = 1
+    # TODO pick one of the available connection classes in homeassistant/config_entries.py
+    CONNECTION_CLASS = config_entries.CONN_CLASS_UNKNOWN
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
+        if user_input is not None:
+            try:
+                info = await validate_input(self.hass, user_input)
+
+                return self.async_create_entry(title=info["title"], data=user_input)
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+
+        return self.async_show_form(
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+        )
+
+
+class CannotConnect(exceptions.HomeAssistantError):
+    """Error to indicate we cannot connect."""
+
+
+class InvalidAuth(exceptions.HomeAssistantError):
+    """Error to indicate there is invalid auth."""
