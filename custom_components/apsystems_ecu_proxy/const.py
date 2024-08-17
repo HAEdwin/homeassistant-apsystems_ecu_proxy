7b063a412e3e37ca4278232498ab29c6e1b69939
HAEdwin <82239374+HAEdwin@users.noreply.github.com>
2024-08-17 15:24:55 +0200

    Update const.py

--- custom_components/apsystems_ecu_proxy/const.py
+++ custom_components/apsystems_ecu_proxy/const.py
@@ -1,4 +1,29 @@
+"""Constants."""
+
+from enum import StrEnum
+
 DOMAIN = "apsystems_ecu_proxy"
+
+# Max age of message (in seconds) to process
+MESSAGE_IGNORE_AGE = 600
+
+# Max summation stub time to add for new period - see code in summation_calculation method in
+# sensor.py.  Added if no messages recieved for sometime due to inverter shutdown.
+MAX_STUB_INTERVAL = 300
+
+# Used to prevent sending data to EMA for testing purposes.  Set to false during testing.
+SEND_TO_EMA = True
+
+ATTR_TIMESTAMP = "timestamp"
+ATTR_SUMMATION_PERIOD = "summation_period"
+ATTR_SUMMATION_TYPE = "summation_type"
+ATTR_SUMMATION_FACTOR = "summation_factor"
+ATTR_CURRENT_VALUE = "value"
+ATTR_INVERTERS = "inverters"
+
+
+MESSAGE_EVENT = f"{DOMAIN}_message"
+
 SOLAR_ICON = "mdi:solar-power"
 FREQ_ICON = "mdi:sine-wave"
 RELOAD_ICON = "mdi:reload"
@@ -6,3 +31,25 @@ CACHE_ICON = "mdi:cached"
 RESTART_ICON = "mdi:restart"
 DCVOLTAGE_ICON = "mdi:current-dc"
 POWER_ICON = "mdi:power"
+
+SOCKET_PORTS = [8995, 8996, 8997]
+EMA_HOST = "3.67.1.32"
+
+
+class SummationPeriod(StrEnum):
+    """Summation period Enum."""
+
+    HOURLY = "hourly"
+    DAILY = "daily"
+    WEEKLY = "weekly"
+    MONTHLY = "monthly"
+    YEARLY = "yearly"
+    LIFETIME = "lifetime"
+
+
+class SummationType(StrEnum):
+    """Summation type Enum."""
+
+    SUM = "sum"
+    MAX = "max"
+    MIN = "min"
