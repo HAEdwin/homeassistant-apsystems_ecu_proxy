760c5ce2e7111ba7619176f6a4024fc1b6db20b0
HAEdwin <82239374+HAEdwin@users.noreply.github.com>
2024-11-04 16:37:25 +0100

    Update config_flow.py

--- custom_components/apsystems_ecu_proxy/config_flow.py
+++ custom_components/apsystems_ecu_proxy/config_flow.py
@@ -9,21 +9,26 @@ from .const import DOMAIN
 _LOGGER = logging.getLogger(__name__)
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """First start of integration settings."""
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
+        """Get stored configuration data to present in async_step_init."""
+        _LOGGER.warning("async_get_options_flow called: %s", config_entry)
         return OptionsFlowHandler(config_entry)
 
     async def async_step_user(self, user_input=None):
+        """First step: Initial set-up of integration options."""
+        _LOGGER.warning("async_step_user")
         schema = vol.Schema({
-            vol.Required("EMA host", default="3.67.1.32"): str,
-            vol.Required("Message ignore age", default="1800"): str,
-            vol.Required("Max stub interval", default="300"): str,
-            vol.Required("No update timeout", default="600"): str,
-            vol.Optional("Send to EMA", default=True): bool,
+            vol.Required("ema_host", default="3.67.1.32"): str,
+            vol.Required("message_ignore_age", default="1800"): str,
+            vol.Required("max_stub_interval", default="300"): str,
+            vol.Required("no_update_timeout", default="600"): str,
+            vol.Required("send_to_ema", default=True): bool,
         })
 
         if user_input is not None:
@@ -36,25 +41,34 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     data_schema=schema,
                     errors={"base": "Could not connect to the specified EMA host."},
                 )
-
         return self.async_show_form(step_id="user", data_schema=schema)
 
+
+
 class OptionsFlowHandler(config_entries.OptionsFlow):
+    """Regular change of integration options."""
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
-        current_options = self.config_entry.options or {}
+        """Second step: Altering the integration options."""
+        current_options = (
+            self.config_entry.data 
+            if not self.config_entry.options 
+            else self.config_entry.options
+        )
+        _LOGGER.warning("async_step_init with options: %s", current_options)
+        
         schema = vol.Schema({
-            vol.Optional("EMA host", default=current_options.get("ema_host", "3.67.1.32")): str,
-            vol.Optional("Message ignore age", default=current_options.get("Message ignore age", "1800")): str,
-            vol.Optional("Max stub interval", default=current_options.get("Max stub interval", "300")): str,
-            vol.Optional("No update timeout", default=current_options.get("No update timeout", "600")): str,
-            vol.Optional("Send to EMA", default=current_options.get("Send to EMA", True)): bool,
+            vol.Required("ema_host", default=current_options.get("ema_host")): str,
+            vol.Required("message_ignore_age", default=current_options.get("message_ignore_age")): str,
+            vol.Required("max_stub_interval", default=current_options.get("max_stub_interval")): str,
+            vol.Required("no_update_timeout", default=current_options.get("no_update_timeout")): str,
+            vol.Required("send_to_ema", default=current_options.get("send_to_ema")): bool,
         })
 
         if user_input is not None:
-            ema_host = user_input["EMA host"]
+            ema_host = user_input["ema_host"]
             if await self.validate_ip(ema_host):
                 updated_options = current_options.copy()
                 updated_options.update(user_input)
@@ -65,7 +79,6 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
                     data_schema=schema,
                     errors={"base": "Could not connect to the specified EMA host."},
                 )
-
         return self.async_show_form(step_id="init", data_schema=schema)
 
     @staticmethod
