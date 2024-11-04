c2da56144b42c77d863b8248be2ae530865dfb94
HAEdwin <82239374+HAEdwin@users.noreply.github.com>
2024-11-04 20:32:59 +0100

    Update config_flow.py

--- custom_components/apsystems_ecu_proxy/config_flow.py
+++ custom_components/apsystems_ecu_proxy/config_flow.py
@@ -8,6 +8,7 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
+
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """First start of integration settings."""
     VERSION = 1
@@ -17,12 +18,12 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @callback
     def async_get_options_flow(config_entry):
         """Get stored configuration data to present in async_step_init."""
-        _LOGGER.warning("async_get_options_flow called: %s", config_entry)
+        _LOGGER.debug("async_get_options_flow called: %s", config_entry)
         return OptionsFlowHandler(config_entry)
 
     async def async_step_user(self, user_input=None):
         """First step: Initial set-up of integration options."""
-        _LOGGER.warning("async_step_user")
+        _LOGGER.debug("async_step_user")
         schema = vol.Schema({
             vol.Required("ema_host", default="3.67.1.32"): str,
             vol.Required("message_ignore_age", default="1800"): str,
@@ -32,9 +33,8 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         })
 
         if user_input is not None:
-            ema_host = user_input["ema_host"]
-            if await OptionsFlowHandler.validate_ip(ema_host):
-                return self.async_create_entry(title="", data=user_input)
+            if await OptionsFlowHandler.validate_ip(user_input["ema_host"]):
+                return self.async_create_entry(title="APsystems ECU proxy", data=user_input)
             else:
                 return self.async_show_form(
                     step_id="user",
@@ -42,7 +42,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     errors={"base": "Could not connect to the specified EMA host."},
                 )
         return self.async_show_form(step_id="user", data_schema=schema)
-
 
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
@@ -57,14 +56,18 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
             if not self.config_entry.options 
             else self.config_entry.options
         )
-        _LOGGER.warning("async_step_init with options: %s", current_options)
+        _LOGGER.debug("async_step_init with options: %s", current_options)
         
+        keys = [
+            "ema_host", 
+            "message_ignore_age", 
+            "max_stub_interval", 
+            "no_update_timeout", 
+            "send_to_ema"
+        ]
         schema = vol.Schema({
-            vol.Required("ema_host", default=current_options.get("ema_host")): str,
-            vol.Required("message_ignore_age", default=current_options.get("message_ignore_age")): str,
-            vol.Required("max_stub_interval", default=current_options.get("max_stub_interval")): str,
-            vol.Required("no_update_timeout", default=current_options.get("no_update_timeout")): str,
-            vol.Required("send_to_ema", default=current_options.get("send_to_ema")): bool,
+            vol.Required(key, default=current_options.get(key)): str if key != "send_to_ema" else bool 
+            for key in keys
         })
 
         if user_input is not None:
@@ -88,6 +91,3 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
             return True
         except (OSError, asyncio.TimeoutError):
             return False
-
-class CannotConnect(exceptions.HomeAssistantError):
-    """Error to indicate we cannot connect."""
